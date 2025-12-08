8f86118ca38a3d16302cf089d3e62293b05eed70
OneNoted@Aeolus <jonatan.jonasson@madeingotland.com>
2025-12-08 10:32:50 +0100

    add preliminary tmux and zsh config

---
+++ dot_config/nvim/lua/plugins/empty_vim-tmux-navigator.lua
@@ -0,0 +1,28 @@
+-- return {
+--   {
+--     "christoomey/vim-tmux-navigator",
+--     lazy = false,
+--     init = function()
+--       vim.g.tmux_navigator_disable_when_zoomed = 1
+--       vim.g.tmux_navigator_preserve_zoom = 1
+--     end,
+--   },
+-- }
+return {
+  "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+    "TmuxNavigatorProcessList",
+  },
+  keys = {
+    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  },
+}
